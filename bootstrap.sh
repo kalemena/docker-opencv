e4992342d0c7c054d4e20ccbb0a5275a1b63f9d1
kalemena <kalemena@free.fr>
2019-01-27 20:49:03 +0100

    Add learn opencv samples

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 export HOME=$HOME; 
 export USER=$USER; 
-
-cd $HOME;
-
-jupyter lab --allow-root --notebook-dir=$HOME --ip='0.0.0.0' --port=8888 --no-browser
+jupyter lab --allow-root --notebook-dir=/notebooks --ip='0.0.0.0' --port=8888 --no-browser
